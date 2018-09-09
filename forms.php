671312004f07480b7d5f920877542d69def5bded
Suraj <surajthekng7@gmail.com>
2018-09-09 19:41:13 +0530

    form creation

---
+++ forms.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="style.css">
+   <link rel="javascript" href="script.js">
+   <title> Project Start</title>
+ </head>
+ <body>
+   <form action="forms.php" method="post">
+     Name&nbsp;<input type="text" alt = "name" placeholder="Full Name"><br><br>
+     Email&nbsp;<input type="text" alt = "email" placeholder="Email"><br><br>
+     <input type="button" value="Submit">
+   </form>
+
+ </body>
+ </html>
